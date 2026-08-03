000b1347b55c11944d245ecaf154f192c254c300
241B012 <241B012@juet.co.in>
2026-08-03 15:54:25 +0530

    add

---
+++ LE2/Add.java
@@ -0,0 +1,12 @@
+class  Add{
+    public int sum(int val1,int val2){
+        return val1+val1;
+        }
+        
+        public void printtable(int n){
+            for(int i=1;i<11;i++){
+                System.out.println(n+"*"+i+":"+i*n);
+                
+            }
+        }
+}
