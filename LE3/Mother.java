8ffecd1addab2359a72c49f84266f8a9526044cf
241B012 <241B012@juet.co.in>
2026-08-10 15:27:46 +0530

    LE3

---
+++ LE3/Mother.java
@@ -0,0 +1,9 @@
+class Mother {
+    Mother(int x) {
+        System.out.println("Mother constructor called");
+    }
+
+    void show() {
+        System.out.println("Mother show method");
+    }
+}
